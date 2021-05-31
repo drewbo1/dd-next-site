68c2e8bcab8cca20cf17bf95a2bb3157bb4efc5a
Andy Marshall <ajmarshall@mail.com>
2021-05-31 16:24:34 +0100

    alter url

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <nav>
       <div className='logo'>
         <Image
-          src='https://res.cloudinary.com/dujftalas/image/upload/v1622472450/logo2black_ckhwkq.png'
+          src='http://res.cloudinary.com/dujftalas/image/upload/v1622472450/logo2black_ckhwkq.png'
           alt='logo'
           width={100}
           height={100}
